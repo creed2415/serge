cb023c15ec8e48f2c9f7be71b3609fe375b9a322
creed2415 <147784107+creed2415@users.noreply.github.com>
2023-11-13 15:16:53 +0800

    Add files via upload
    gpu support

---
+++ scripts/dev-gpu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+# Handle termination signals
+_term() {
+	echo "Received termination signal!"
+	kill -TERM "$redis_process" 2>/dev/null
+	kill -TERM "$serge_process" 2>/dev/null
+}
+
+# Install python bindings
+UNAME_M=$(dpkg --print-architecture) CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python || {
+	echo 'Failed to install llama-cpp-python'
+	exit 1
+}
+
+# Start Redis instance
+redis-server /etc/redis/redis.conf &
+redis_process=$!
+
+# Start the API
+cd /usr/src/app/api || exit 1
+uvicorn src.serge.main:app --host 0.0.0.0 --port 8008 || {
+	echo 'Failed to start main app'
+	exit 1
+} &
+serge_process=$!
+
+# Set up a signal trap and wait for processes to finish
+trap _term TERM
+wait $redis_process $serge_process
